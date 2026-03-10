9c83a2b2b4d2c49de92f41d9aee97e0dc58e5f15
rafakunzel <rafafernandes1950@gmail.com>
2026-03-09 21:12:18 -0300

    adiciona aula1.py

---
+++ aula1.py/1.py
@@ -0,0 +1,21 @@
+print('olá mundo!')
+
+pokebola='bulbasaur'
+pokebola1='psyduck'
+pokebola2='clefairy'
+
+idade=30
+nome='Maria'
+preco=19.99
+data='4-3-2026'
+
+nota1=8
+nota2=6
+media=(nota1+nota2)/2
+soma=nota1+nota2
+subs=nota1-nota2
+mult=nota1*nota2
+div=nota1/nota2
+mod=nota1%nota2
+# print(media)
+print(f'{soma}\n{subs}\n{mult}\n{div}\n{mod}')
---
+++ aula1.py/2.py
@@ -0,0 +1,32 @@
+# not1=5
+# not2=6
+# not3=7
+# media=(not1+not2+not3)/3
+# if media>7:
+#     print(f'aluno aprovado media={media}')
+# elif 5<media<7:
+#     print(f'aluno em recuperação media={media}')
+# else:
+#     print(f'aluno reprovado media={media}')
+
+
+# frase = "Python é divertido"
+# print(frase.upper())
+# nova_frase = frase.replace("divertido", "poderoso") #substitu divertido por poderoso
+# print(nova_frase)
+
+# contador = 0
+# contador += 5 # contador=contador+5
+# contador -= 2 # contador=contador-2
+# contador *= 3 # contador=contador*3
+# print('Valor final do contador:', contador)
+
+a = 10
+b = 20
+c = 30
+media = (a + b + c) / 3
+print("Média:", media)
+print("A média é maior que 15 e menor que 25?", 15 < media < 25)
+
+nota = int(input("Nota:"))
+print(nota)
