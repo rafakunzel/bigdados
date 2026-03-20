a4d01c4b435e1b83cee62e8965e466303b78bc45
rafakunzel <rafafernandes1950@gmail.com>
2026-03-20 19:15:13 -0300

    adicione a aula

---
+++ aula1.py/5_7.py
@@ -0,0 +1,15 @@
+# lista=[] #É a estrutura mais flexível e comum.
+# tupla=() #lista imutável não tem como alterar nada 
+# set=() # elimina duplicatas e verifica presença de itens rapidamente ordem não é garantida
+# dict1={:} # armazenando informações em pares Chave: Valor (como um índice de telefone ou um registro de cliente).
+# #dict1= dicionário  parecido com JSON
+# nomesdic={'nome1':'Matheus',
+# 'nome2':'Alice',
+# 'nome3':'Caio',
+# 'nome4':'Larissa',
+# 'nome5':'Miguel',
+# 'nome6':'Rafael'}
+# nomes=['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+# print(nomes)
+
+print('Ola bibuihvgvhvjhytyuvbchctctyyctyctyc')
