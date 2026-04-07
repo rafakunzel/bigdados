a754751d09f289e5e7df453bac114b3049a7e1eb
Rafael Fernandes <rafafernandes1950@gmail.com>
2026-04-06 21:48:33 -0300

    03.sql
    conectar sql ao python

--- parte2/03.sql
+++ parte2/03.sql
@@ -9,3 +9,24 @@ preco decimal(10,2),
 estoque int
 );
 Select * From produto;
+
+conectar sql no python 
+import mysql.connector
+#conectar o sql no python
+conexao = mysql.connector.connect(
+ host="127.0.0.1",
+ user="root",
+ password="",
+ database="online_vendas"
+)
+# 2. Criar um objeto cursor para executar as queries
+cursor = conexao.cursor()
+# 3. Definir a query
+query = "SELECT * FROM produto"
+# 4. Executar a query
+cursor.execute(query)
+# 5. Obter os resultados
+resultados = cursor.fetchall()
+# 6. Exibir os resultados
+for linha in resultados:
+ print(linha)
