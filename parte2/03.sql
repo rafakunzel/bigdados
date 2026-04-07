9353f2d7c2a6d7d265afd616c5e8545ddd821a57
Rafael Fernandes <rafafernandes1950@gmail.com>
2026-04-06 21:29:18 -0300

    03.sql

--- parte2/03.sql
+++ parte2/03.sql
@@ -1,3 +1,11 @@
 -- Criação de banco de dados relacionais
-CREATE DATABASE vendas_online;
-CREATE TABLE vendas_online;
+CREATE DATABASE online_vendas;
+USE online_vendas;
+CREATE TABLE Produto (
+id_produto int primary key not null auto_increment,
+nome_produto varchar(100),
+categoria varchar(50),
+preco decimal(10,2),
+estoque int
+);
+Select * From produto;
